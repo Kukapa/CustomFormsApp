640d7c444c3858b110441781d0c041c5530b2b60
Maksim Gornostaev <maksim.gornostaev197@gmail.com>
2024-10-02 22:32:13 +0300

    Update ControllerRoute

--- Program.cs
+++ Program.cs
@@ -22,6 +22,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Form}/{action=Index}/{id?}");
 
 app.Run();
