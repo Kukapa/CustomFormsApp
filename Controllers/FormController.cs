37c1b8ddf9e655705abbacdbd2779e0d86e5a8df
Maksim Gornostaev <maksim.gornostaev197@gmail.com>
2024-10-02 21:57:20 +0300

    add FormController

---
+++ Controllers/FormController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CustomFormsApp.Controllers
+{
+    public class FormController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Submit(string name, string email)
+        {
+            ViewData["Name"] = name;
+            ViewData["Email"] = email;
+            return View("Result");
+        }
+    }
+}
