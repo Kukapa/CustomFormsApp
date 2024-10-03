c93199b2bee0aeefa95e6a0ba0fcc59e84aa4a34
Maksim Gornostaev <maksim.gornostaev197@gmail.com>
2024-10-03 21:22:25 +0300

    Create TemplateController

---
+++ Controllers/TemplateController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using CustomFormsApp.Models;
+
+namespace CustomFormsApp.Controllers
+{
+    public class TemplateController : Controller
+    {
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View(new TemplateModel());
+        }
+
+        [HttpPost]
+        public IActionResult Create(TemplateModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                // Logic to save the template (this will be done using EF later)
+                TempData["SuccessMessage"] = "Template created successfully!";
+                return RedirectToAction("Index", "Home");
+            }
+
+            return View(model);
+        }
+    }
+}
