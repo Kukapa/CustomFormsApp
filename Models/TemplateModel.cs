577a38bd4e147ae2c1511255d0c10512bad08425
Maksim Gornostaev <maksim.gornostaev197@gmail.com>
2024-10-03 21:25:40 +0300

    Create TemplateModel

---
+++ Models/TemplateModel.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace CustomFormsApp.Models
+{
+    public class TemplateModel
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Title is required.")]
+        public string Title { get; set; }
+
+        [Required(ErrorMessage = "Description is required.")]
+        public string Description { get; set; }
+
+        public string Tags { get; set; } // For tag-based search functionality later
+
+        public bool IsPublic { get; set; } // Determines if the template is public or private
+
+        public List<QuestionModel> Questions { get; set; } = new List<QuestionModel>(); // Questions associated with the template
+    }
+}
