0639ef8e802e632f1487ee99bc9009c1a2b0db0e
Maksim Gornostaev <maksim.gornostaev197@gmail.com>
2024-10-03 21:25:30 +0300

    Create QuestionModel

---
+++ Models/QuestionModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CustomFormsApp.Models
+{
+    public class QuestionModel
+    {
+        public int Id { get; set; }
+        public int TemplateId { get; set; }
+
+        [Required(ErrorMessage = "Question title is required.")]
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        [Required(ErrorMessage = "Please select the type of the question.")]
+        public string QuestionType { get; set; } // Single-line, multi-line, integer, checkbox
+
+        public bool IsVisibleInResults { get; set; } = true; // Can be toggled by the user
+    }
+}
