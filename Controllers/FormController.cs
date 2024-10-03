0d124eb04bd4982df8580f2a3423d93690647b99
Maksim Gornostaev <maksim.gornostaev197@gmail.com>
2024-10-03 21:23:44 +0300

    Add Database features

--- Controllers/FormController.cs
+++ Controllers/FormController.cs
@@ -1,27 +1,50 @@
-﻿using CustomFormsApp.Models;
+﻿using CustomFormsApp.Data;
+using CustomFormsApp.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace CustomFormsApp.Controllers
 {
     public class FormController : Controller
     {
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly ApplicationDbContext _context;
+
+        public FormController(UserManager<IdentityUser> userManager, ApplicationDbContext context)
+        {
+            _userManager = userManager; // Add this line
+            _context = context;
+        }
+
+        [HttpGet]
         public IActionResult Index()
         {
-            return View();
+            return View(new FormModel());
         }
 
         [HttpPost]
+        [ValidateAntiForgeryToken]
         public IActionResult Submit(FormModel model)
         {
             if (ModelState.IsValid)
             {
-                return RedirectToAction("Result", model);
-            }
+                var userId = _userManager.GetUserId(User);
+                model.UserId = userId;
+                _context.Forms.Add(model);
 
-            return View("Index", model);
+                TempData["SuccessMessage"] = "Form submitted successfully.";
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                TempData["ErrorMessage"] = "There were validation errors.";
+                return View("Index", model);
+            }
         }
 
-        public IActionResult Result(FormModel model)
+        [HttpGet]
+        public IActionResult Result(FormModel model) 
         {
             return View(model);
         }
