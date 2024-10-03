4c66935faa095e8631371d19a689cda4c87e524b
Maksim Gornostaev <maksim.gornostaev197@gmail.com>
2024-10-03 21:24:19 +0300

    Add ApplicationDbContext file

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using CustomFormsApp.Models;
+
+namespace CustomFormsApp.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<TemplateModel> Templates { get; set; }
+        public DbSet<QuestionModel> Questions { get; set; }
+        public DbSet<FormModel> Forms { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
