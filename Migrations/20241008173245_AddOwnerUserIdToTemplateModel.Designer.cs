561a5074234ffd06feda0719dd10470d962f2fcd
Maksim Gornostaev <maksim.gornostaev197@gmail.com>
2024-10-09 18:46:54 +0300

    Add OwnerUserId migration

--- Migrations/20241008173245_AddOwnerUserIdToTemplateModel.Designer.cs
+++ Migrations/20241008173245_AddOwnerUserIdToTemplateModel.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using CustomFormsApp.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace CustomFormsApp.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20241008173245_AddOwnerUserIdToTemplateModel")]
+    partial class AddOwnerUserIdToTemplateModel
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Migrations/20241008173245_AddOwnerUserIdToTemplateModel.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CustomFormsApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddOwnerUserIdToTemplateModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Questions_Templates_TemplateModelId",
+                table: "Questions");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Questions_TemplateModelId",
+                table: "Questions");
+
+            migrationBuilder.DropColumn(
+                name: "TemplateModelId",
+                table: "Questions");
+
+            migrationBuilder.AddColumn<string>(
+                name: "OwnerUserId",
+                table: "Templates",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Questions_TemplateId",
+                table: "Questions",
+                column: "TemplateId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Questions_Templates_TemplateId",
+                table: "Questions",
+                column: "TemplateId",
+                principalTable: "Templates",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Questions_Templates_TemplateId",
+                table: "Questions");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Questions_TemplateId",
+                table: "Questions");
+
+            migrationBuilder.DropColumn(
+                name: "OwnerUserId",
+                table: "Templates");
+
+            migrationBuilder.AddColumn<int>(
+                name: "TemplateModelId",
+                table: "Questions",
+                type: "integer",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Questions_TemplateModelId",
+                table: "Questions",
+                column: "TemplateModelId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Questions_Templates_TemplateModelId",
+                table: "Questions",
+                column: "TemplateModelId",
+                principalTable: "Templates",
+                principalColumn: "Id");
+        }
+    }
+}
