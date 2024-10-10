17faf86d7f212cd42d3ed78750c4c9d9301a25eb
Maksim Gornostaev <maksim.gornostaev197@gmail.com>
2024-10-10 21:57:08 +0300

    Create UserRole

---
+++ Models/UserRoleViewModel.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace CustomFormsApp.Models
+{
+    public class UserRoleViewModel
+    {
+        public IdentityUser User { get; set; }
+        public IList<string> Roles { get; set; }
+    }
+}
