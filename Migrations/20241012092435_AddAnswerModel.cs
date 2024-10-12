56e0d8f743dad1910d6f875e4944ebd96a158e7d
Maksim Gornostaev <maksim.gornostaev197@gmail.com>
2024-10-12 15:56:23 +0300

    Add AnswerModel

---
+++ Migrations/20241012092435_AddAnswerModel.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace CustomFormsApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddAnswerModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Answers",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    QuestionId = table.Column<int>(type: "integer", nullable: false),
+                    UserId = table.Column<string>(type: "text", nullable: false),
+                    AnswerText = table.Column<string>(type: "text", nullable: false),
+                    AnswerInteger = table.Column<int>(type: "integer", nullable: true),
+                    AnswerBoolean = table.Column<bool>(type: "boolean", nullable: true),
+                    SubmittedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Answers", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Answers_Questions_QuestionId",
+                        column: x => x.QuestionId,
+                        principalTable: "Questions",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Answers_QuestionId",
+                table: "Answers",
+                column: "QuestionId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Answers");
+        }
+    }
+}
---
+++ Models/AnswerModel.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CustomFormsApp.Models
+{
+    public class AnswerModel
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public int QuestionId { get; set; }
+
+        [Required]
+        public string UserId { get; set; } 
+
+        public string AnswerText { get; set; }
+
+        public int? AnswerInteger { get; set; } 
+        
+        public bool? AnswerBoolean { get; set; }
+
+        public DateTime SubmittedAt { get; set; }
+
+        public QuestionModel Question { get; set; }
+    }
+}
