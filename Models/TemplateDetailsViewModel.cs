5793ec3994da29fc48a2b55c2d3d48684e18b95b
Maksim Gornostaev <maksim.gornostaev197@gmail.com>
2024-10-19 15:57:29 +0300

    Change property

--- Models/TemplateDetailsViewModel.cs
+++ Models/TemplateDetailsViewModel.cs
@@ -5,7 +5,6 @@
         public TemplateModel Template { get; set; }
         public bool CanManageTemplate { get; set; } 
         public bool IsAdmin { get; set; }
-
-        public List<AnswerModel> FormResults { get; set; }
+        public List<FilledFormModel> FormResults { get; set; }
     }
 }
