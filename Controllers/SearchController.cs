1bfd783a9c37ee9e17dcc5511f693c0ef5547939
Maksim Gornostaev <maksim.gornostaev197@gmail.com>
2024-10-20 13:34:01 +0300

    Add Search button

---
+++ Controllers/SearchController.cs
@@ -0,0 +1,37 @@
+﻿using CustomFormsApp.Data;
+using CustomFormsApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace CustomFormsApp.Controllers
+{
+    public class SearchController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public SearchController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public async Task<IActionResult> Results(string query)
+        {
+            if (string.IsNullOrEmpty(query))
+            {
+                return View(new List<TemplateModel>());
+            }
+
+            var templates = await _context.Templates
+                .Where(t => t.Title.Contains(query) || t.Description.Contains(query) ||
+                            t.Questions.Any(q => q.Title.Contains(query))) // || t.Comments.Any(c => c.Text.Contains(query)))
+                .Include(t => t.Questions)
+                .ToListAsync();
+
+            return View(templates);
+        }
+    }
+}
